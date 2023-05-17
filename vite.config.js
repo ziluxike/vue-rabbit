dd62a4bfb47e9224cc2acc5a62b3129524d3831e
ziluxike <leimujiang233@gmail.com>
2023-05-18 01:17:55 +0800

    更改主题色

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// elementPlus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [
+            ElementPlusResolver({ importStyle: "sass" })
+        ]
+      }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+      preprocessorOptions: {
+          scss: {
+              additionalData: `
+              @use "@/styles/element/index.scss" as *;
+              `,
+          }
+      }
   }
 })
